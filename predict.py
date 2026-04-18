8dc62e7e1b5678a21f1fe2c08dd83888969d158d
Yashaswini-1810 <yashaswinimitta0@gmail.com>
2026-04-18 08:49:13 +0530

    Improve model download and prediction error handling
    Refactor model loading and error handling in prediction function.

--- predict.py
+++ predict.py
@@ -2,23 +2,21 @@ import os
 import gdown
 import numpy as np
 import tensorflow as tf
+from huggingface_hub import hf_hub_download
 
+# MODEL_PATH = hf_hub_download(
+#     repo_id="your-username/plant-model",
+#     filename="plant_model.h5"
+# )
 MODEL_PATH = "plant_model.h5"
+
+# model = tf.keras.models.load_model(MODEL_PATH, compile=False)
 
 file_id = "1MC6_8cqW_YSii6BD6YbyYBV3QLkkKloX"
 url = f"https://drive.google.com/uc?id={file_id}"
-
-# download safely
-if not os.path.exists(MODEL_PATH):
-    print("Downloading model...")
-    gdown.download(url, MODEL_PATH, quiet=False)
-
-# CHECK if file exists BEFORE loading
-if not os.path.exists(MODEL_PATH):
-    raise Exception("Model download failed!")
-
+if not os.path.exists("plant_model.h5"):
+    gdown.download(url, "plant_model.h5", quiet=False)
 model = tf.keras.models.load_model(MODEL_PATH, compile=False)
-
 class_names = [
     "Apple___Cedar_apple_rust",
     "Tomato___Late_blight",
@@ -27,11 +25,15 @@ class_names = [
 ]
 
 def predict_image(image):
-    image = image.resize((224, 224))
-    img = np.array(image) / 255.0
-    img = np.expand_dims(img, axis=0)
+    try:
+        image = image.resize((224, 224))
+        img = np.array(image) / 255.0
+        img = np.expand_dims(img, axis=0)
 
-    pred = model.predict(img)
-    index = np.argmax(pred)
+        pred = model.predict(img)
+        index = np.argmax(pred)
 
-    return class_names[index], float(np.max(pred))
+        return class_names[index], float(np.max(pred))
+
+    except Exception as e:
+        return "Error", 0.0
